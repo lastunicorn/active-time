27d4705c3d89c1092db77ad918facad8bc3623d7
lastunicorn <lastunicorn@f72a2467-73a6-491e-be61-0da19ad49aed>
2011-07-02 16:49:12 +0000

    git-svn-id: https://svn.code.sf.net/p/activetime/code/trunk@13 f72a2467-73a6-491e-be61-0da19ad49aed

---
+++ sources/ActiveTime/Recording/DayRecord.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace DustInTheWind.ActiveTime.Recording
+{
+    public class DayRecord
+    {
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private Record[] records;
+
+        public Record[] Records
+        {
+            get { return records; }
+            set { records = value; }
+        }
+
+        private string comment;
+
+        public string Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return (records == null || records.Length == 0) && (comment == null || comment.Length == 0); }
+        }
+
+        public bool HasRecords
+        {
+            get { return records != null && records.Length > 0; }
+        }
+
+        public bool HasComment
+        {
+            get { return comment != null && comment.Length > 0; }
+        }
+
+        public TimeSpan GetTotalTime()
+        {
+            TimeSpan totalTime = TimeSpan.Zero;
+
+            if (records != null)
+            {
+                foreach (Record record in records)
+                {
+                    totalTime += record.EndTime - record.StartTime;
+                }
+            }
+
+            return totalTime;
+        }
+
+        public TimeSpan GetIntervalTime()
+        {
+            TimeSpan totalTime = TimeSpan.Zero;
+
+            if (records != null)
+            {
+                TimeSpan beginHour = records[0].StartTime;
+                TimeSpan endHour = records[0].EndTime;
+
+                foreach (Record record in records)
+                {
+                    if (record.StartTime < beginHour)
+                        beginHour = record.StartTime;
+
+                    if (record.EndTime > endHour)
+                        endHour = record.EndTime;
+                }
+
+                totalTime = endHour - beginHour;
+            }
+
+            return totalTime;
+        }
+
+        public TimeSpan? GetBeginTime()
+        {
+            if (records != null && records.Length > 0)
+            {
+                return records[0].StartTime;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ sources/ActiveTime/Recording/RecorderState.cs
@@ -0,0 +1,19 @@
+﻿
+namespace DustInTheWind.ActiveTime.Recording
+{
+    /// <summary>
+    /// Represents the state of a <see cref="Recorder"/> instance.
+    /// </summary>
+    public enum RecorderState
+    {
+        /// <summary>
+        /// The recorder is stopped.
+        /// </summary>
+        Stopped,
+
+        /// <summary>
+        /// The recorder is running and updating the values in the database.
+        /// </summary>
+        Running
+    }
+}
