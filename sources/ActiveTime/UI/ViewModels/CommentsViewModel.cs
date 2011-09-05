a74cd9efa3c4a9884abb0656555408f7ad318f27
lastunicorn <lastunicorn@f72a2467-73a6-491e-be61-0da19ad49aed>
2011-09-05 22:47:28 +0000

    Created new CommentsViewModel class and unit tests for it. Not finished.
    git-svn-id: https://svn.code.sf.net/p/activetime/code/trunk@37 f72a2467-73a6-491e-be61-0da19ad49aed

---
+++ sources/ActiveTime/UI/ViewModels/CommentsViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DustInTheWind.ActiveTime.Persistence.Repositories;
+using System.ComponentModel;
+using DustInTheWind.ActiveTime.Persistence.Entities;
+
+namespace DustInTheWind.ActiveTime.UI.ViewModels
+{
+    class CommentsViewModel : INotifyPropertyChanged
+    {
+        private IDayCommentRepository commentRepository;
+
+        private DateTime date = DateTime.Today;
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Date"));
+            }
+        }
+
+        private string comment;
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                comment = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("Comment"));
+            }
+        }
+
+        private bool commentTextWrap = true;
+        public bool CommentTextWrap
+        {
+            get { return commentTextWrap; }
+            set
+            {
+                commentTextWrap = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CommentTextWrap"));
+            }
+        }
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+        #endregion
+
+        public CommentsViewModel(IDayCommentRepository dayCommentRepository)
+        {
+            if (dayCommentRepository == null)
+                throw new ArgumentNullException("dayCommentRepository");
+
+            this.commentRepository = dayCommentRepository;
+        }
+
+        public void WindowLoaded()
+        {
+            try
+            {
+                DayComment dayComment = commentRepository.GetByDate(date);
+                Comment = dayComment == null ? string.Empty : dayComment.Comment;
+            }
+            catch
+            {
+                //view.DisplayError(ex);
+            }
+
+        }
+    }
+}
